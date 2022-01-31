4336c554537620963b81a3f346ae6faffd0ffc67
Galygina Svetlana <svetlanka.woman@gmail.com>
2022-01-31 18:21:02 +0200

    next qwestion

--- js/script.js
+++ js/script.js
@@ -1,12 +1,36 @@
 window.addEventListener('DOMContentLoaded', () => {
   "use strict";
 
+  const indicator = document.querySelectorAll('.indicator'),
+        numberTotal = document.querySelectorAll('.number__total'),
+        question = document.querySelectorAll('.question');
+
+  question.forEach((item, i) => {
+    if (i > 0) {
+      item.classList.add('hide');
+    }
+    numberTotal[i].textContent = question.length-1;
+    for (let k = 0; k < i+1; k++) {
+      indicator[i].innerHTML += '<li class="indicator_black"></li>';
+    }
+    for (let k = 0; k < question.length-i-2; k++) {
+      indicator[i].innerHTML += '<li></li>';
+    }
+  }) 
+
   const ranges = document.querySelectorAll('.range');
   const arrGradient = ['#ba1417','#cd0800','#f45800','#fd9113', '#ffb800', '#d7e317', '#d1e01f', '#d1e01f', '#79E371', '#00ab23', '#00ab23'];
 
   ranges.forEach(range => {
     const rangeValue = range.nextElementSibling,
-          sliderRange = range.parentElement.previousElementSibling;
+          sliderRange = range.parentElement.previousElementSibling,
+          answerName = range.getAttribute('name'),
+          answerExtendedLike = document.querySelector(`.answer-extended.${answerName}.liked`),
+          checkboxLike = document.querySelectorAll(`.answer-extended.${answerName}.liked .answer-checkbox`),
+          answerExtendedDisappointed = document.querySelector(`.answer-extended.${answerName}.disappointed`),
+          checkboxDisappointed = document.querySelectorAll(`.answer-extended.${answerName}.disappointed .answer-checkbox`);
+
+    range.value = 0;
 
     const showColorRange = () => {
       rangeValue.style.display = 'block';
@@ -18,9 +42,10 @@ window.addEventListener('DOMContentLoaded', () => {
       sliderRange.style.cssText = `
         -webkit-mask: url('../img/thumb-line.svg') ${+range.value * 10}% 0/60px 26px, linear-gradient(to right, #fff calc(50% - 60px/2 + 1px), transparent 0 calc(50% + 60px/2 - 1px), #fff 0) right ${+range.value * 10}% top 21px/calc(200% - 60px) 4px;
         -webkit-mask-repeat: no-repeat;
-        mask: url('../img/thumb-line.svg') 0% 0/60px 26px, linear-gradient(to right, #fff calc(50% - 60px/2 + 1px), transparent 0 calc(50% + 60px/2 - 1px), #fff 0) right 0% top 21px/calc(200% - 60px) 4px;
+        mask: url('../img/thumb-line.svg') ${+range.value * 10}% 0/60px 26px, linear-gradient(to right, #fff calc(50% - 60px/2 + 1px), transparent 0 calc(50% + 60px/2 - 1px), #fff 0) right ${+range.value * 10}% top 21px/calc(200% - 60px) 4px;
         mask-repeat: no-repeat;
         `;
+        
       if (range.value == 0 && rangeValue.textContent == 1) {
         rangeValue.style.left = '7.5%';
       } else {
@@ -32,15 +57,29 @@ window.addEventListener('DOMContentLoaded', () => {
 
     range.addEventListener('click', () => {
       showColorRange();
+      if (answerName == 'answer_1') {
+        question[1].classList.remove('hide');
+        question[1].classList.add('show', 'fade');
+      }
     });
 
     range.addEventListener('change', () => {
-      if (range.value < 7) {
-console.log(range.getAttribute('name'));
-      }
-    })
+      if (answerName !== 'answer_1') {
+        if (range.value > 6) {
+          answerExtendedLike.classList.add('show', 'fade');
+          answerExtendedDisappointed.classList.remove('show', 'fade');
+          checkboxDisappointed.forEach(checkbox => {
+            checkbox.checked = false;
+          });
+        } else {
+          answerExtendedDisappointed.classList.add('show', 'fade');
+          answerExtendedLike.classList.remove('show', 'fade');
+          checkboxLike.forEach(checkbox => {
+            checkbox.checked = false;
+          });
+        }
+      } 
+    });
   });
-
-
 
 });
